fc6190a7de02f03533e4ca7017b8465f61e737ca
François Schwarzentruber <francois.schwarzentruber@ens-rennes.fr>
2020-04-16 21:18:46 +0200

    cleaning

--- main.py
+++ main.py
@@ -4,24 +4,22 @@ import os
 def recognize(wavpath):
     try:
         # Recognize audio
-        r = speech_recognition.Recognizer()
+        recognizer = speech_recognition.Recognizer()
         with speech_recognition.AudioFile(wavpath) as source:
-            audio = r.record(source)  # read the entire audio file
+            audio = recognizer.record(source)  # read the entire audio file
 
     except Exception as ex:
         print(str(ex))
 
-    recognized_text = ''
-
     # recognize speech using Google_STT --> We'll see if we continue with another STT engine
     try:
-        recognized_text = r.recognize_google(audio, language='fr-FR')
-        print("Google thinks you said:\n\n" + format_recognized_text(recognized_text) + "\n\n")
+        return format_recognized_text(recognizer.recognize_google(audio, language='fr-FR'))
     except speech_recognition.UnknownValueError:
         print("Google could not understand audio")
+        return ""
     except speech_recognition.RequestError as e:
         print("Google error; {0}".format(e))
-    return recognized_text 
+        return ""
 
 def format_recognized_text(p_str):
 #   With Google STT engine:
@@ -36,5 +34,6 @@ def format_recognized_text(p_str):
 
 if __name__ == "__main__":
     os.system('arecord -r 44100 file.wav')
-    recognize('./file.wav')
+    recognized_text = recognize('./file.wav')
+    print("Google thinks you said:\n\n" + recognized_text + "\n\n")     
     os.remove('./file.wav')
