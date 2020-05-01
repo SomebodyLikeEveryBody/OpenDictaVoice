3fbfc0303506cb71c007a33ece15210aa717ade7
r0b-levrai <r0b@kali.kali-dom>
2020-05-01 23:00:29 +0200

    stop record function launched in Thread. Now we need to display the Moulinage icon while sound is parsed par STT

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ def launch_record_in_thread(p_recorder):
     thread_record = threading.Thread(target=p_recorder.start_record)
     thread_record.start()
 
+def stop_record_in_thread(p_recorder, p_voice_recognizer, p_formatter, p_filename):
+    thread_stop_record = threading.Thread(target=stop_record_then_analyse, args=(p_recorder, p_voice_recognizer, p_formatter, p_filename))
+    thread_stop_record.start()
 
 def stop_record_then_analyse(p_recorder, p_voice_recognizer, p_formatter, p_filename):
     p_recorder.stop_record_N_save(p_filename)
@@ -35,7 +38,7 @@ def main():
     gui.rec_button.bind("<Button-1>", lambda event: [gui.switch_buttons(
         event), voice_recognizer.set_language(gui.get_language()), launch_record_in_thread(recorder)])
     gui.stop_button.bind("<Button-1>", lambda event: [gui.switch_buttons(
-        event), stop_record_then_analyse(recorder, voice_recognizer, formatter, WAV_FILENAME)])
+        event), stop_record_in_thread(recorder, voice_recognizer, formatter, WAV_FILENAME)])
     gui.launch()
 
 
