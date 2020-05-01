f7db0a87926d41bd603b57b27826f1afa8e81d7d
r0b-levrai <r0b@kali.kali-dom>
2020-05-01 11:47:07 +0200

    synchronized version

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ def launch_record_in_thread(p_recorder):
     thread_record.start()
 
 def stop_record_then_analyse(p_recorder, p_voice_recognizer, p_gui, p_filename):
-def analyse_wav_to_get_text(p_filename):
     recognized_text = recognize_wav(p_filename)
     print(recognized_text)
     pyperclip.copy(recognized_text)
@@ -23,7 +22,7 @@ def analyse_wav_to_get_text(p_filename):
     #pyautogui.hotkey('ctrl', 'v')   
     os.remove(p_filename)
 
-def stop_record_then_analyse(p_recorder, p_filename):
+def stop_record_then_analyse(p_recorder, p_voice_recognizer, p_filename):
     p_recorder.stop_record_N_save(p_filename)
     p_voice_recognizer.wav_to_formated_text(p_filename)
 
@@ -33,7 +32,7 @@ def main():
     gui = opendictavoice_modules.GUI.builded_GUI(RESOURCES_PATH)
 
     gui.rec_button.bind("<Button-1>", lambda event : [gui.switch_buttons(event), launch_record_in_thread(recorder)])
-    gui.stop_button.bind("<Button-1>", lambda event : [gui.switch_buttons(event), stop_record_then_analyse(recorder, voice_recognizer, gui, WAV_FILENAME)])
+    gui.stop_button.bind("<Button-1>", lambda event : [gui.switch_buttons(event), stop_record_then_analyse(recorder, voice_recognizer, WAV_FILENAME)])
     gui.launch()
 
 if __name__ == "__main__":
