32df4a2bba8e5d1a5122d4b1f1eb4c53c5444863
François Schwarzentruber <francois.schwarzentruber@ens-rennes.fr>
2020-05-06 12:44:56 +0200

    formatter

--- main.py
+++ main.py
@@ -22,21 +22,23 @@ def stop_record_then_analyse_in_thread(p_audio_manager, p_voice_recognizer, p_fo
 def stop_record_then_analyse(p_audio_manager, p_voice_recognizer, p_formatter, p_filename):
     p_audio_manager.stop_record_N_save(p_filename)
     text = p_voice_recognizer.wav_to_text(p_filename)
+    formatedText = p_formatter.format(text)
     print('\n\n=========================')
     print("text that was recognized: " + text)
-    print("text formatted: " + p_formatter.format(text))
+    print("text formatted: " + formatedText)
     print("(I put it in your editor)")
     print('=========================\n\n')
     #pyperclip.copy(text)
 #    pyautogui.PAUSE = 0.4
 #    pyautogui.hotkey('alt', 'tab')
 #    pyautogui.hotkey('ctrl', 'v')
-    pynput.keyboard.Controller().type(text)
+    pynput.keyboard.Controller().type(formatedText)
     
     
     
 #same function but with "alt tab" to give the focus to the window you are working in.
 #used when the user clicks on the button (and is not triggering the recording via his/her keyboard)
+#I DISLIKE HAVING DUPLICATED THE CODE
 def stop_record_then_analyse_and_give_focus_in_thread(p_audio_manager, p_voice_recognizer, p_formatter, p_filename):
     thread_stop_record = threading.Thread(target=stop_record_then_analyse_and_give_focus, args=(p_audio_manager, p_voice_recognizer, p_formatter, p_filename))
     thread_stop_record.start()
@@ -44,20 +46,22 @@ def stop_record_then_analyse_and_give_focus_in_thread(p_audio_manager, p_voice_r
 def stop_record_then_analyse_and_give_focus(p_audio_manager, p_voice_recognizer, p_formatter, p_filename):
     p_audio_manager.stop_record_N_save(p_filename)
     text = p_voice_recognizer.wav_to_text(p_filename)
+    formatedText = p_formatter.format(text)
     print('\n\n=========================')
     print("text that was recognized: " + text)
-    print("text formatted: " + p_formatter.format(text))
+    print("text formatted: " + formatedText)
     print("(I put it in your editor)")
     print('=========================\n\n')
     pyautogui.PAUSE = 0.2
     pyautogui.hotkey('alt', 'tab')
-    pynput.keyboard.Controller().type(text)
+    pynput.keyboard.Controller().type(formatedText)
 
 def main():
     gui = opendictavoice_modules.builded_GUI.Builded_GUI(RESOURCES_PATH)
     audio_manager = opendictavoice_modules.audio_manager.Audio_manager(RESOURCES_PATH)
     voice_recognizer = opendictavoice_modules.voice_recognizer.Voice_Recognizer(audio_manager)
-    formatter = opendictavoice_modules.formatter.Formatter([RESOURCES_PATH + 'rewritingrules/LaTEX.txt', RESOURCES_PATH + 'rewritingrules/basic.txt'])
+    formatter = opendictavoice_modules.formatter.Formatter([RESOURCES_PATH + 'rewritingrules/LaTEX.txt',
+                                                            RESOURCES_PATH + 'rewritingrules/basic.txt'])
 
     def start_rec(p_event=None):
         gui.stop_button_visible()
