a99a846dae02a699d08db85114ac44b18dc2d460
r0b3rt[0] <nullepart@rien.com>
2020-05-18 23:27:20 +0200

    started to encapsulate attributes of class to proper

--- opendictavoice_modules/builded_GUI.py
+++ opendictavoice_modules/builded_GUI.py
@@ -6,13 +6,22 @@ WINDOW_HEIGHT = 150
 class Builded_GUI:
     def __init__(self, p_resources_path):
         self.resources_path = p_resources_path
-        self.window = self.build_window()
+        self._window = self.build_window()
         self.rec_button = self.build_rec_button()
         self.stop_button = self.build_stop_button()
         self.language_chooser_menu = self.build_language_chooser()
 
         self.language_chooser_menu.pack()
         self.rec_button.pack()
+
+    @property
+    def window(self):
+        raise PermissionError("It is not authorized to access or modify [window] attribute")
+        return None
+
+    @window.setter
+    def window(self, p_value):
+        raise PermissionError("It is not authorized to access or modify [window] attribute")
 
     def build_window(self):
         ret_window = tkinter.Tk()
@@ -27,11 +36,11 @@ class Builded_GUI:
 
     def on_closing(self):
         print("AU REVOIR")
-        self.window.destroy()
+        self._window.destroy()
 
     def build_rec_button(self):
         img = tkinter.PhotoImage(file=self.resources_path + 'imgs/record.png')
-        ret_button = tkinter.Button(self.window, image=img)
+        ret_button = tkinter.Button(self._window, image=img)
         ret_button.image = img
         ret_button.config(width=WINDOW_WIDTH, height=WINDOW_HEIGHT) 
 
@@ -39,7 +48,7 @@ class Builded_GUI:
 
     def build_stop_button(self):
         img = tkinter.PhotoImage(file=self.resources_path + 'imgs/stop_record.png')
-        ret_button = tkinter.Button(self.window, image=img)
+        ret_button = tkinter.Button(self._window, image=img)
         ret_button.image = img
         ret_button.config(width=WINDOW_WIDTH, height=WINDOW_HEIGHT)
 
@@ -47,9 +56,9 @@ class Builded_GUI:
 
     def build_language_chooser(self):
         choices = { 'fr','en'}
-        language_stringvar = tkinter.StringVar(self.window)
+        language_stringvar = tkinter.StringVar(self._window)
         language_stringvar.set('fr') 
-        ret_menu = tkinter.OptionMenu(self.window, language_stringvar, *choices)
+        ret_menu = tkinter.OptionMenu(self._window, language_stringvar, *choices)
         ret_menu.language_stringvar = language_stringvar
         return ret_menu
         
@@ -62,7 +71,7 @@ class Builded_GUI:
         self.stop_button.pack()
 
     def launch(self):
-        self.window.mainloop()
+        self._window.mainloop()
 
     #returns the string that represents the current language, e.g. "fr" or "en"
     def get_language(self):
