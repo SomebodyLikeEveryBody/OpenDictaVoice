d99a8e661af0bba36a1cbcf21e8fa99b7d395eec
r0b3rt[0] <nullepart@rien.com>
2020-06-07 17:39:07 +0200

    create setup.py to build binary to differents plateforms

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from distutils.core import setup
 
 setup(name='OpenDictaVoice',
       version='1.0',
-      py_modules=['main.py'],
+      py_modules=['opendictavoice_main.py'],
 	install_requires=[]
       )
